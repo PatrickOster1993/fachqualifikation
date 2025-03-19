636f8e5f610e998e937d6c5dbff1ae58f2fc0513
Murat <playonstake0@gmail.com>
2025-03-19 12:01:16 +0100

    added lists.py

--- lists.py
+++ lists.py
@@ -58,3 +58,65 @@ if 'a' in d:
     print("a befindet sich in d-Liste!")
 else:
     print("Kein a in d-Liste vorhanden!")
+
+# Listenelemente in einer Schleife durchlaufen
+i = 0
+while i < 10: # Kopfgesteuerte While Schleife
+    print(i)
+    i += 1 # i = i + 1 --> ++1
+
+i = 0
+while True: # Endlos Schleife # Fußgesteuerte While Schleife
+    print(i)
+    i += 1
+    if i >= 10:
+        break
+
+# Jetzt auf Listen / Arrays angewandt
+meine_liste = ["Hallo","ich","heisse","Murat"]
+
+i = 0
+while i < len(meine_liste):
+    print(meine_liste[i])
+    i += 1
+
+# Reverse
+i = len(meine_liste) - 1
+while i >= 0:
+    print(meine_liste[i])
+    i -= 1
+
+# Flexible Verwendung von Listen
+
+hey = [3, "hallo", True]
+print(hey)
+print(type(hey)) # class = list
+
+# for-Schleife mit vielen Ausprägungen
+for i in range(0, len(meine_liste)):
+    print(meine_liste[i])
+
+# oder viel kompakter
+for inhalt in meine_liste:
+    print(inhalt)
+
+# Weitere wichtige Grundlagen bzgl. Arrays / Listen
+
+neue_liste = [1, 2, 3, 4]
+
+## Letzes Element der liste entfernen
+
+neue_liste.pop() # Entfernt die Position aus dem Index
+print(neue_liste)
+
+## Elemente an beliebiger Stelle hinzufügen
+
+neue_liste.insert(1, 10) # (index, einZufügendeZahl)
+print(neue_liste)
+
+neue_liste.remove(10) # Entfernt den Wert aus der Liste
+print(neue_liste)
+
+neue_liste.reverse() # Dreht die Liste um
+print(neue_liste)
+
