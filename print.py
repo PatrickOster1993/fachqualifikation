a83a337d549c02a99f1acca831e7dc096c687869
Patrick Oster <p.oster9330@googlemail.com>
2025-03-19 08:16:22 +0100

    initial commit

---
+++ print.py
@@ -0,0 +1,10 @@
+hallo = "Hallo"
+name = input("Dein Name: ")
+print(hallo + " " + name)
+
+print("hallo "); print("welt!") # bad practice!!!
+
+# a = 5
+# print("Meine Zahl ist", a, sep='#')
+# print("Meine Zahl ist", a)
+# print("Meine Zahl ist" + "5")
---
+++ variables.py
@@ -0,0 +1,108 @@
+# Numerisch
+x = 1_000_000
+y = 1
+z = x + y
+
+print("Ergebnis z =", z)
+
+hex_zahl = 0x1
+print("Hexadezimal:", hex(hex_zahl))
+
+oct_zahl = 0o1
+print("Oktalzahl:", oct(oct_zahl))
+
+bin_zahl = 0b1011
+print("Binärzahl:", bin(bin_zahl))
+
+dez_zahl = 11
+print("Binärzahl aus Dezimalzahl:", bin(dez_zahl))
+
+print("#################")
+# String
+name = "Patrick"
+print(len(str(3)))
+
+# Typen-Check
+x = None
+print("Typ:", type(None))
+手动阀 = 3
+
+print(手动阀)
+
+# ID checken
+i = 3
+n = 5
+print(id(i), id(n))
+
+# Logische Operatoren (binär)
+# &   |   ^
+
+# 1110 1001
+# &
+# 1101 1011
+# ---------
+# 1100 1001
+
+binary_one = 0b101
+binary_two = 0b111
+
+x = 4
+y = 3
+z = x & y
+
+
+# 0b101
+# &
+# 0b111
+# -----
+# 0b101
+
+print("Dez. Lösung:", z)
+
+binary_result = binary_one & binary_two
+
+print("Lösung:", binary_result)
+
+# == != < > <= >=
+
+# Bedingungen
+
+a = True
+b = False
+c = 3
+d = 5
+e = 3
+
+if a:
+    print("1. If-Statement wahr!")
+elif a:
+    print("elif wahr!")
+else:
+    print("1. If-Statement falsch!")
+
+if c != 3:
+    print("2. if-Statement wahr!")
+    
+else:
+    print("2. If-Statement falsch!")
+
+
+# Shift-Operator
+
+zahl1 = 0b1001
+
+zahl2 = zahl1 << 1
+
+# 0000 0111
+# 0000 0100
+# &
+# 0000 0100 >> 1
+
+print("Zahl 1:", bin(zahl1))
+print("Zahl 2:", bin(zahl2))
+
+
+
+
+
+
