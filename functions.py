0eeca6790a1a48459e5b8ecc455ecf731df57bb2
Murat <playonstake0@gmail.com>
2025-03-19 12:55:44 +0100

    added functions.py

---
+++ functions.py
@@ -0,0 +1,9 @@
+# Funtkion zum Addieren 
+def addiere(a, b):
+    ergebnis = a + b
+    return ergebnis
+
+x = int(input("Zahl 1: "))
+y = int(input("Zahl 2: "))
+
+print(f"Ergebnis der Addition: {addiere(x, y)}") # Ausführung der Funktion
