2033cd3bcc18cc1debec3306f368a1451635328e
paco <pascal.stein@gmail.com>
2025-03-19 09:45:32 +0100

    initial

--- print.py
+++ print.py
@@ -7,4 +7,4 @@ print("hallo "); print("welt!") # bad practice!!!
 # a = 5
 # print("Meine Zahl ist", a, sep='#')
 # print("Meine Zahl ist", a)
-# print("Meine Zahl ist" + "5")
+# print("Meine Zahl ist" + "5") 
