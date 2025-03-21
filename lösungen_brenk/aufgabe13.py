11a4e4545f807017c83bc88c1d3bdfa1bf63ea26
benediktbrenk <benedikt.brenk@outlook.de>
2025-03-21 14:25:55 +0100

    aufgabe 13 verbesserung

--- lösungen_brenk/aufgabe13.py
+++ lösungen_brenk/aufgabe13.py
@@ -1,4 +1,10 @@
-tic_tac_toe = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+####### DATA #######
+tic_tac_toe = [
+    ["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "9"]
+    ]
+
 winning_conditions = [
     [[0, 0], [0, 1], [0, 2]],
     [[1, 0], [1, 1], [1, 2]],
@@ -10,23 +16,16 @@ winning_conditions = [
 
     [[0, 0], [1, 1], [2, 2]],
     [[0, 2], [1, 1], [2, 0]]
-]
+    ]
 
-print("---------")
-for i in range(3):
-    print("|", end=" ")
-    for j in range(3):
-        print(tic_tac_toe[i][j], end=" ")
-    print("|", end=" ")
-    print()
-print("---------")
+players = [
+    ["player_1", "X"],
+    ["player_2", "O"]
+    ]
 
-while True:
+####### FUNCTIONS #######
 
-    player1_x = int(input("Player 1 (X), please tell me the x-coordinate."))
-    player1_y = int(input("Player 1 (X), please tell me the y-coordinate."))
-    tic_tac_toe[player1_y][player1_x] = "X"
-    
+def show_board():
     print("---------")
     for i in range(3):
         print("|", end=" ")
@@ -36,33 +35,49 @@ while True:
         print()
     print("---------")
 
-    player_wins = False
-    
-    for i in range(len(winning_conditions)):
+def move(player, sign):
+    player_x = int(input(f"{player} ({sign}), please tell me the x-coordinate."))
+    player_y = int(input(f"{player} ({sign}), please tell me the y-coordinate."))
+    tic_tac_toe[player_y][player_x] = sign
 
+def check_winning_conditions():
+    win = False
+
+    for i in range(len(winning_conditions)):
         for condition in winning_conditions[i]:
         
             x, y = condition
             
-            if tic_tac_toe[x][y] == "X":
-                player_wins = True
+            if tic_tac_toe[x][y] == players[0][1] or tic_tac_toe[x][y] == players[1][1]:
+                win = True
             else:
-                player_wins = False
+                win = False
+    
+    return win
 
+def round(player, sign):
+    move(player, sign)
+    show_board()
+
+    player_wins = check_winning_conditions()
+    
     if player_wins == True:
-        print("Player 1 wins.")
+        print(f"{player} wins.")
+        return "win"
+    else: 
+        return None
+
+####### PROGRAM #######
+
+show_board()
+
+while True:
+
+    result = round(players[0][0], players[0][1])
+    if result == "win":
         break
 
-    player2_x = int(input("Player 2 (O), please tell me the x-coordinate."))
-    player2_y = int(input("Player 2 (O), please tell me the y-coordinate."))
-    tic_tac_toe[player2_y][player2_x] = "O"
-    
-    print("---------")
-    for i in range(3):
-        print("|", end=" ")
-        for j in range(3):
-            print(tic_tac_toe[i][j], end=" ")
-        print("|", end=" ")
-        print()
-    print("---------")
+    result = round(players[1][0], players[1][1])
+    if result == "win":
+        break
 
